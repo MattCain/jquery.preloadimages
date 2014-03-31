6ee54e101a51a0cdfd45351ad736064d49aa4a5f
Matt Cain <matt@cainmedia.com>
2014-03-31 21:21:05 +0100

    Changed license contact info

--- jquery.preloadimages.js
+++ jquery.preloadimages.js
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2014 Matt Cain (@CainCode)
+/*!
+ * Copyright (c) 2014 Matt Cain (Twitter:@CainCode, Github:@MattCain)
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
